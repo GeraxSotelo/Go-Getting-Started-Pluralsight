df21fce3e64cf25131aac26bfbc734adc93d428f
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-06 10:45:30 -0600

    working with pointers

--- main.go
+++ main.go
@@ -27,4 +27,20 @@ func main() {
 	//real & img functions
 	r, i := real(c), imag(c);
 	fmt.Println(r, i);
+
+	//Pointer data type. A variable will point to another location in memory that's going to hold the relevant information
+	//lastName is going to be a pointer to a string value. It must be initialized before assigning anything to it
+	var lastName *string = new(string)
+	//Dereference operation (*). Dereferencing is reaching through the pointer, grabing the data, and bringing it back
+	*lastName = "Sotelo";
+	fmt.Println(*lastName);
+
+	//Address of operator (&). Go allows to create & declare a variable, then create a pointer that points to the variable.
+	middleInit := "Nef";
+	ptr := &middleInit;
+	fmt.Println(ptr, *ptr);
+
+	middleInit = "N";
+	//The value of the pointer 'ptr' changes, but the memory address doesn't. The 'middleInit' variable is still in the same location
+	fmt.Println(ptr, *ptr);
 }
