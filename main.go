a33f7d62149c03f8fce1af86156b7d0eeb967732
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-06 09:33:35 -0600

    declaring variables with primitive data types

--- main.go
+++ main.go
@@ -5,5 +5,26 @@ import (
 )
 
 func main() {
-	fmt.Println("test")
+	//Initialize in separate lines. var keyword, variable name, type
+	var a int;
+	a = 42
+	fmt.Println(a)
+
+	//Initialize on same line. With floating-point variables, size must be specified. (float32, float64)
+	var f float32 = 3.14
+	fmt.Println(f);
+
+	//Implicit initialization syntax. Using :=  Go will imply the data type
+	firstName := "Gerax"
+	fmt.Println(firstName)
+
+	b := true;
+	fmt.Println(b);
+
+	c := complex(3, 4);
+	fmt.Println(c);
+	
+	//real & img functions
+	r, i := real(c), imag(c);
+	fmt.Println(r, i);
 }
