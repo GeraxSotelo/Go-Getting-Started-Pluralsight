1773947a6d84c4b78810ec27512451571a193e6a
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-08 08:33:22 -0600

    using methods to add behaviors to a type

--- models/user.go
+++ models/user.go
@@ -12,8 +12,23 @@ var (
 	//users is a slice that's going to hold pointers to User objects
 	//collection of Users
 	users []*User
-	
+
 	//At the package level there's no need to provide the colon operator to get the implicit initialization syntax.
 	//Here, the compiler will imply the int data type.
 	nextID = 1
 )
+
+//GetUsers return a slice of pointers to User objects
+func GetUsers() []*User {
+	return users;
+}
+
+//AddUser returns 2 values, the User created and a potential error object
+func AddUser (u User) (User, error) {
+	u.ID = nextID;
+	nextID++;
+	//use the address of operator (&) to grab a reference to the user object that came in. 
+	//(The users collection is storing pointers to User objects, so we need the address of the user object that came in and store it)
+	users = append(users, &u);
+	return u, nil; //returning nil because no error occurrs here
+}
