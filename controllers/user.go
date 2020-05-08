bb72a99816d7db99686b17b6d31863bf9389734c
Gerax Sotelo <gsotelo23@gmail.com>
2020-05-08 11:05:35 -0600

    create constructor function

--- controllers/user.go
+++ controllers/user.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"regexp"
 	"net/http"
 )
 
-type userController struct {}
+type userController struct {
+	userIDPattern *regexp.Regexp
+}
 
 //variable name - uc
 //type to bind to - userController
@@ -12,4 +15,17 @@ type userController struct {}
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	//convert string to byte slice
 	w.Write([]byte("Hello from User Controller"));
+}
+
+//Go doesn't use constructors, it uses constructor functions
+//Convention in naming a constructor function is starting with the word 'new', then the name of the type of object that will be constructed
+//often returns a pointer to the object to avoid an unnecessary copy operation
+func newUserController() *userController {
+	//When dealing with struct data types, constructing an object and using the 'address of' operator (&) without a named variable is permissible. You can immediately take the address of it
+	return &userController{
+		//regex will look for paths that are '/users/ a number'
+		userIDPattern: regexp.MustCompile(`/users/(\d+)/?`),
+	}
+	//This is actually a local variable. We're constructing userController in the scope of this function & returning the address of it
+	//Go will recognize if we're returning the address of a local variable & automatically promote the variable up to the level that it needs to be so we don't lose the variable & we don't lose track of the memory that was being allocated by that function
 }
